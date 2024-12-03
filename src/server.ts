f2c3467b81c9408374cc2aacda4eaba82ca48711
pedrocarvalho3 <pdrpvp0310@gmail.com>
2024-12-03 08:31:02 -0300

    Adição host em 'server.ts'

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { env } from './env'
 import { app } from './app'
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server Running!')
 })
